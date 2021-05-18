71d8e6cda6f7891938bf83de68c0e165d52ac9fc
g3or3 <>
2021-05-18 16:13:11 -0400

    complete

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -1,63 +1,43 @@
-// Require `checkUsernameFree`, `checkUsernameExists` and `checkPasswordLength`
-// middleware functions from `auth-middleware.js`. You will need them here!
+const bcrypt = require("bcryptjs");
+const Users = require("../users/users-model");
+const router = require("express").Router();
 
+const {
+	checkUsernameFree,
+	checkUsernameExists,
+	checkPasswordLength,
+} = require("./auth-middleware");
 
-/**
-  1 [POST] /api/auth/register { "username": "sue", "password": "1234" }
+router.post("/register", checkUsernameFree, checkPasswordLength, async (req, res) => {
+	res.json(
+		await Users.add({
+			username: req.body.username,
+			password: await bcrypt.hash(req.body.password, 8),
+		})
+	);
+});
 
-  response:
-  status 200
-  {
-    "user_id": 2,
-    "username": "sue"
-  }
+router.post("/login", checkUsernameExists, async (req, res) => {
+	if (await bcrypt.compare(req.body.password, req.existingUser.password)) {
+		req.session.user = req.existingUser;
+		res.json({ message: `Welcome ${req.existingUser.username}!` });
+	} else {
+		res.json({ status: 401, message: "Invalid credentials" });
+	}
+});
 
-  response on username taken:
-  status 422
-  {
-    "message": "Username taken"
-  }
+router.get("/logout", (req, res, next) => { //eslint-disable-line
+	if (req.session.user) {
+		req.session.destroy((err) => {
+			if (err) {
+				console.log(err);
+			} else {
+				res.json({ message: "logged out" });
+			}
+		});
+	} else {
+		res.json({ status: 200, message: "no session" });
+	}
+});
 
-  response on password three chars or less:
-  status 422
-  {
-    "message": "Password must be longer than 3 chars"
-  }
- */
-
-
-/**
-  2 [POST] /api/auth/login { "username": "sue", "password": "1234" }
-
-  response:
-  status 200
-  {
-    "message": "Welcome sue!"
-  }
-
-  response on invalid credentials:
-  status 401
-  {
-    "message": "Invalid credentials"
-  }
- */
-
-
-/**
-  3 [GET] /api/auth/logout
-
-  response for logged-in users:
-  status 200
-  {
-    "message": "logged out"
-  }
-
-  response for not-logged-in users:
-  status 200
-  {
-    "message": "no session"
-  }
- */
-
- 
-// Don't forget to add the router to the `exports` object so it can be required in other modules
+module.exports = router;
